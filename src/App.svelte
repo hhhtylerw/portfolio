5b1ff77d77f2d479f9415fe62d9fd3fe102abb18
h3x <hhhtylerw@gmail.com>
2022-08-09 10:55:14 -0400

    add gif icon
    fix title
    remove readme
    fix exports

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,8 @@
 <script>
-	export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h1>Hello h3x!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
