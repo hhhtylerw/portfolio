49dfea55de27c84e28fbf7266b64e6c24f91738e
hhhtylerw <hhhtylerw@gmail.com>
2022-08-09 23:29:12 -0400

    layout change

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,68 @@
 <script>
 </script>
 
+<!-- Main Content -->
 <main>
-	<div class="header">Header</div>
-	<div class="sidebar">Left</div>
-	<div class="center">Center</div>
-	<div class="sidebar">Right</div>
+	<!-- Header -->
+	<header class="header">
+		Header
+		<p>
+		Lorem ipsum dolo    
+		</p>
+	</header>
+
+	<!-- Hero -->
+	<section class="hero">
+		Hero
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sumenda potius quam expetenda. Nihil opus est exemplis hoc facere longius.
+		</p>
+	</section>
+
+	<div class="flex-container wrapper">
+
+		<!-- Content -->
+		<section class="content">
+			Content
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sumenda potius quam expetenda. Nihil opus est exemplis hoc facere longius. At Zeno eum non beatum modo, sed etiam divitem dicere ausus est. Nunc ita separantur, ut disiuncta sint, quo nihil potest esse perversius. Itaque quantum adiit periculum! ad honestatem enim illum omnem conatum suum referebat, non ad voluptatem. Ergo id est convenienter naturae vivere, a natura discedere. Esse enim quam vellet iniquus iustus poterat inpune. Tu enim ista lenius, hic Stoicorum more nos vexat. Duo Reges: constructio interrete.
+			</p>
+			<p>
+				Quam ob rem tandem, inquit, non satisfacit? Haec quo modo conveniant, non sane intellego. Ita ceterorum sententiis semotis relinquitur non mihi cum Torquato, sed virtuti cum voluptate certatio. Tubulum fuisse, qua illum, cuius is condemnatus est rogatione, P. Quem ad modum quis ambulet, sedeat, qui ductus oris, qui vultus in quoque sit? Miserum hominem! Si dolor summum malum est, dici aliter non potest. Quo tandem modo? Ex rebus enim timiditas, non ex vocabulis nascitur. Deinde prima illa, quae in congressu solemus: Quid tu, inquit, huc? Quod eo liquidius faciet, si perspexerit rerum inter eas verborumne sit controversia.
+			</p>                
+		</section>
+
+		<!-- Sidebar -->
+		<aside class="sidebar">
+			Sidebar                
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sumenda potius quam expetenda. Nihil opus est exemplis hoc facere longius. At Zeno eum non beatum modo, sed etiam divitem dicere ausus est. Nunc ita separantur, ut disiuncta sint, quo nihil potest esse perversius. Itaque quantum adiit periculum!
+			</p>
+		</aside>
+
+	</div>
+
+	<!-- Footer -->
+	<footer class="footer">
+		Footer                
+		<p>
+			Esse enim quam vellet iniquus iustus poterat inpune. Tu enim ista lenius, hic Stoicorum more nos vexat. Duo Reges: constructio interrete.
+		</p>
+	</footer>
 
 </main>
 
 <style>
-	main {
-		color: white;
-	}
-	.header {
-		width: 100%;
-	}
-	.center {
-		width: 60%;
-		min-height: 80%;
-		float: left;
-		background-color: antiquewhite;
-	}
-	.sidebar {
-		width: 20%;
-		min-height: 80%;
-		float: left;
-		background-color: #333;
-	}
+*,:after,:before{box-sizing:border-box}
+html{font-size:100%}
+body{margin:0;padding:20px;font-family:Arial,Helvetica,sans-serif;font-size:1rem;color:#fff}
+.wrapper{margin:auto;max-width:75rem}
+aside,footer,header,main,section{display:block;margin:0;padding:1.25rem;border:1px solid #000;color:#fff}
+
+.header{background:#03a9f4}
+.hero{background:#d22b1f}
+@media screen and (min-width:640px){.flex-container{display:flex}}
+.content{flex:1;background:#129a22}.sidebar{flex:0 1 300px;background:#673ab7}
+.footer,.sidebar{border:1px solid #000}
+.footer{background:#616161}
 </style>
