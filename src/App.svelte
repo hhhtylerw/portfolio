c295653bdac28ea24b38d323aadd639b8bcfa2d1
h3x <hhhtylerw@gmail.com>
2022-08-11 11:40:17 -0400

    major color scheme changes

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,8 @@
 <main>
 	<!-- Header -->
 	<header class="header">
-		<img src="https://media.discordapp.net/attachments/774535774913429504/1006949391552237638/coollogo_com-9700947.png" alt="my name" style="display:block;margin-left:auto;margin-right: auto;min-width: 50%;max-width:100%">
+		<!--<img src="https://media.discordapp.net/attachments/774535774913429504/1006949391552237638/coollogo_com-9700947.png" alt="my name" style="display:block;margin-left:auto;margin-right: auto;min-width: 50%;max-width:100%">-->
+		Tyler Woodruff
 	</header>
 
 	
@@ -14,13 +15,13 @@
 		<!-- Sidebar -->
 		<div class="sidebar">
 			<aside class="sidebarinfo">
-				About Me               
+				>About               
 				<p>
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sumenda potius quam expetenda. Nihil opus est exemplis hoc facere longius. At Zeno eum non beatum modo, sed etiam divitem dicere ausus est. Nunc ita separantur, ut disiuncta sint, quo nihil potest esse perversius. Itaque quantum adiit periculum!
 				</p>
 			</aside>
 			<aside class="sidebarinfo">
-				Contacting Me                
+				>Contact               
 				<table style="margin:10px 0 0 0;">
 					<tr>
 						<td>Email: hhhtylerw@gmail.com</td>
@@ -41,7 +42,7 @@
 				</table>
 			</aside>
 			<aside class="sidebarinfo">
-				My Education
+				>Education
 				<p>
 					I am currently attending the University of Florida and completing a Bachelor's in Computer Science. I am expected to graduate in May 2024.
 				</p>
@@ -51,7 +52,7 @@
 		<!-- Content -->
 		<div class="content">
 			<section class="contentinfo">
-				Experience
+				>Experience
 				<p>
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sumenda potius quam expetenda. Nihil opus est exemplis hoc facere longius. At Zeno eum non beatum modo, sed etiam divitem dicere ausus est. Nunc ita separantur, ut disiuncta sint, quo nihil potest esse perversius. Itaque quantum adiit periculum! ad honestatem enim illum omnem conatum suum referebat, non ad voluptatem. Ergo id est convenienter naturae vivere, a natura discedere. Esse enim quam vellet iniquus iustus poterat inpune. Tu enim ista lenius, hic Stoicorum more nos vexat. Duo Reges: constructio interrete.
 				</p>
@@ -60,7 +61,7 @@
 				</p>                
 			</section>
 			<section class="contentinfo">
-				Projects
+				>Projects
 				<p>
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sumenda potius quam expetenda. Nihil opus est exemplis hoc facere longius. At Zeno eum non beatum modo, sed etiam divitem dicere ausus est. Nunc ita separantur, ut disiuncta sint, quo nihil potest esse perversius. Itaque quantum adiit periculum! ad honestatem enim illum omnem conatum suum referebat, non ad voluptatem. Ergo id est convenienter naturae vivere, a natura discedere. Esse enim quam vellet iniquus iustus poterat inpune. Tu enim ista lenius, hic Stoicorum more nos vexat. Duo Reges: constructio interrete.
 				</p>
@@ -72,15 +73,15 @@
 		</div>
 	</div>
 	
-	<!-- Right Dolphin -->
+	<!-- Right Dolphin 
 	<div>
 		<img src="https://images-ext-1.discordapp.net/external/oQhYVctHm6VkxC1G66npb7KfEsuLWYJ8C5Odqwwxhoo/https/i.pinimg.com/originals/af/fb/a2/affba24abd5d2eef5090a9e15f400acc.gif?width=279&height=463" alt="Right Dolphin" style="position:fixed;bottom:0;right:0">
-	</div>
+	</div> -->
 
-	<!-- Left Dolphin -->
+	<!-- Left Dolphin 
 	<div>
 		<img src="https://images-ext-1.discordapp.net/external/oQhYVctHm6VkxC1G66npb7KfEsuLWYJ8C5Odqwwxhoo/https/i.pinimg.com/originals/af/fb/a2/affba24abd5d2eef5090a9e15f400acc.gif?width=279&height=463" alt="Left Dolphin" style="position:fixed;bottom:0;left:0;">
-	</div>
+	</div> -->
 
 	<!-- Footer -->
 	<footer class="footer">
@@ -92,14 +93,14 @@
 <style>
 *,:after,:before{box-sizing:border-box}
 .wrapper{margin:auto;max-width:75rem}
-aside,footer,header,main,section{display:block;margin:0;padding:1.25rem;border:1px solid #000;color:#fff}
+aside,footer,header,main,section{display:block;margin:0;padding:1.25rem;border:2px solid #213c53;color:#fff}
 main{padding:0;margin:0;border:0}
-.header{border: 0}
+.header{border:0;text-align: center; font-size: 80px;font-weight: 900;}
 @media screen and (min-width:640px){.flex-container{display:flex}}
 .content{flex:1;}
-.contentinfo{flex:1;background:#252526;margin:10px 5px}
+.contentinfo{flex:1;background:transparent;margin:10px 5px}
 .sidebar{flex:0 1 300px;}
-.sidebarinfo{flex:1;background:#323133;margin:10px 5px}
-.footer{border:1px solid #000;background:#616161;position:fixed;bottom:0;left:0;right:0;padding:0;text-align:center;}
+.sidebarinfo{flex:1;background:transparent;margin:10px 5px}
+.footer{border:2px solid #213c53;background:transparent;padding:0;text-align:center;}
 a{color:aqua;background-color: none;}
 </style>
